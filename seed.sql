f16df9ef52922db71b298ecd46ae9b8df2024ae9
Noodles <charles.pitek@gmail.com>
2020-01-27 13:58:41 -0700

    set up the basic database stuff for this project

---
+++ seed.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE department(
+    id INT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+    id INT,
+    title VARCHAR(30),
+    salary DECIMAL(8,2),
+    department_id INT,
+    PRIMARY KEY(id)
+);
+
+CREAT TABLE employee(
+    id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NULL,
+    PRIMARY KEY(id)
+);
+
